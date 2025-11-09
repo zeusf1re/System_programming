cd950956d260097bf52038846538c3b53535c677
zeusf1re <akski@yahoo.com>
2025-11-09 17:24:39 +0300

    Almost full DZ_1

--- DZ_1/lib.asm
+++ DZ_1/lib.asm
@@ -1,19 +1,22 @@
-format ELF64
+format elf64
+
 public InitHeap
 public PushBack
 public PopHead
 public FillRand
+public PrintOdd
+public CountEven
 
 ; Переименованная структура, чтобы избежать конфликта с FASM
 S_Queue.head equ 0
 S_Queue.tail equ 8
 
-section '.data' readable
+section '.data' writable
 	urandomPath db '/dev/urandom', 0
 
 section '.bss' writable
-	p_heapStart resq 1
-	p_heap resq 1
+	p_heapStart rq 1
+	p_heap rq 1
 
 section '.text' executable
 
@@ -84,36 +87,147 @@ PopHead:
         ret
 
 ; rdi - S_Queue*
-FillRand: 
-    pushaq
-    
+FillRand:
+    ; --- Сохраняем только те "callee-saved" регистры, которые мы используем ---
+    push rbx
+    push r12
+    push r13
+
+    ; Сохраняем указатель на очередь в r13
     mov r13, rdi
 
+    ; Открываем файл
     mov  rax, 2
     mov  rdi, urandomPath
     xor  rsi, rsi
     syscall
-    mov  r12, rax
+    mov  r12, rax ; fd в r12
 
-    mov  rbx, [r13 + S_Queue.head] 
+    ; Начинаем итерацию
+    mov  rbx, [r13 + S_Queue.head]
+
+.loop:
+    cmp  rbx, 0
+    je   .end_loop
+
+    ; Читаем в узел
+    mov  rax, 0
+    mov  rdi, r12   ; Перезаписываем "расходный" rdi, это нормально
+    mov  rsi, rbx
+    mov  rdx, 8
+    syscall
+
+    ; Переходим к следующему узлу
+    mov  rbx, [rbx + 8]
+    jmp  .loop
+
+.end_loop:
+    ; Закрываем файл
+    mov  rax, 3
+    mov  rdi, r12
+    syscall
+
+    ; --- Восстанавливаем регистры в обратном порядке ---
+    pop r13
+    pop r12
+    pop rbx
+    ret
+
+;rdi = Queue*
+CountEven:
+    push rbx
+    push r10
+    xor rax, rax
+    mov rbx, [rdi] ; node* (head)
 
     .loop:
-        cmp  rbx, 0
-        je   .end_loop
+        cmp rbx, 0
+        je .end
 
-        mov  rax, 0
-        mov  rdi, r12
-        mov  rsi, rbx
-        mov  rdx, 8
-        syscall        
+        mov r10, [rbx] ; r10 int (value) 
+        test r10, 1
+        jnz .loop_inc
+        inc rax
 
-        mov  rbx, [rbx + 8]
-        jmp  .loop
+    .loop_inc:
+        mov rbx, [rbx + 8] ; rbx node* (tail)
+        jmp .loop
 
-    .end_loop:
-        mov  rax, 3
-        mov  rdi, r12
-        syscall
-
-        popaq
+    .end:
+        pop r10
+        pop rbx
         ret
+
+
+; rdi = Queue*
+PrintOdd:
+    push rbp
+    mov rbp, rsp
+    push rbx
+    push r12
+    push r13
+    push r14
+
+    ; Выделяем буфер на 32 байта.
+    sub rsp, 32
+
+    mov rbx, [rdi] ; rbx = q->head
+
+.mailLoop:
+    test rbx, rbx
+    jz .end
+    
+    mov r10, [rbx] ; r10 = current_node->value 
+    test r10, 1
+    jz .mainLoop_inc  ; Пропускаем четные
+
+    ;print
+    mov rax, r10
+    mov r11, 10
+    xor r12, r12      ; r12  счетчик символов
+
+    .divLoop:
+        xor rdx, rdx
+        div r11 
+        add rdx, '0'
+        push rdx      
+        inc r12
+        test rax, rax
+        jnz .divLoop
+
+    lea r13, [rbp - 64] ; 64 потом что сохранили 4 регистра пушами(каждый по 8) а потом выделили еще 32
+    
+    mov r14, r12      ; сохраням r12 - counter
+
+    .popLoop:
+        pop rax       
+        mov [r13], al 
+        inc r13
+        dec r12
+        jnz .popLoop
+        
+    mov byte [r13], 10 ;\n
+    inc r14
+
+    mov rax, 1
+    mov rdi, 1
+    lea rsi, [rbp - 64]  ; lea в целом как move, только не вычисляет значение, то есть юзает только адресса
+    mov rdx, r14       
+    syscall
+    
+    jmp .mainLoop_inc
+
+.mainLoop_inc:
+    mov rbx, [rbx + 8] ; current_node = current_node->next
+    jmp .mailLoop
+
+.end:
+    add rsp, 32      
+    pop r14
+    pop r13
+    pop r12
+    pop rbx
+    pop rbp
+    ret
+
+
--- DZ_1/main.c
+++ DZ_1/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h> // malloc  free
+#include <stdlib.h>
 
 typedef struct Node {
     long value;
@@ -15,12 +15,14 @@ extern void InitHeap(void* ptr, long size);
 extern void PushBack(Queue* q, long value);
 extern long PopHead(Queue* q);
 extern void FillRand(Queue* q);
+extern long CountEven(Queue* q);
+//extern long CountEndsByOne(Queue* q);
+extern void PrintOdd(Queue* q);
 
 int main() {
-    const long HEAP_SIZE = 65536; // 64KB
+    const long HEAP_SIZE = 65536;
     void* heapMemory = malloc(HEAP_SIZE);
     if (heapMemory == NULL) {
-        printf("Ошибка: не удалось выделить память для кучи!\n");
         return 1;
     }
 
@@ -30,41 +32,25 @@ int main() {
     myQueue.head = NULL;
     myQueue.tail = NULL;
 
-    printf("--- Демонстрация PushBack и PopHead ---\n");
-    printf("Добавляем 5, 10, 15\n");
-    PushBack(&myQueue, 5);
-    PushBack(&myQueue, 10);
-    PushBack(&myQueue, 15);
-
-    long val = PopHead(&myQueue);
-    printf("Извлечено: %ld (ожидаем 5)\n", val);
-
-    val = PopHead(&myQueue);
-    printf("Извлечено: %ld (ожидаем 10)\n", val);
-    
-    PopHead(&myQueue); // извлекаем 15
-    printf("\nОчередь очищена.\n");
-
-    printf("\n--- Демонстрация FillRand ---\n");
-    printf("Добавляем 1, 2, 3\n");
     PushBack(&myQueue, 1);
-    PushBack(&myQueue, 2);
-    PushBack(&myQueue, 3);
+    PushBack(&myQueue, 12);
+    PushBack(&myQueue, 21);
+    PushBack(&myQueue, 33);
+    PushBack(&myQueue, 44);
+    PushBack(&myQueue, 51);
+    PushBack(&myQueue, 68);
+    PushBack(&myQueue, 71);
     
-    printf("Заменяем значения на случайные...\n");
-    FillRand(&myQueue);
+    printf("Количество четных: %ld\n", CountEven(&myQueue));
+ //   printf("Количество оканчивающихся на 1: %ld\n", CountEndsByOne(&myQueue));
+    
+    printf("Нечетные числа из очереди:\n");
+    PrintOdd(&myQueue);
 
-    printf("Извлекаем три случайных значения:\n");
-    val = PopHead(&myQueue);
-    printf("Извлечено: %ld\n", val);
-    val = PopHead(&myQueue);
-    printf("Извлечено: %ld\n", val);
-    val = PopHead(&myQueue);
-    printf("Извлечено: %ld\n", val);
-
-    val = PopHead(&myQueue);
-    printf("\nПробуем извлечь из пустой очереди: %ld (ожидаем -1)\n", val);
-
+    while (myQueue.head != NULL) {
+        PopHead(&myQueue);
+    }
+    
     free(heapMemory);
 
     return 0;
